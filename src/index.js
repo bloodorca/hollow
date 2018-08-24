463356bbbb11851e41c7b88696a63e9518add528
Crementif <26669564+Crementif@users.noreply.github.com>
2018-08-24 14:27:20 +0200

    Add Full Nintendo Switch support
    Also supports converting between switch and other platforms.

--- src/index.js
+++ src/index.js
@@ -28,21 +28,31 @@ class App extends React.Component {
     handleFileChange = files => {
         if (files.length > 0){
             let file = files[0]
-            let reader = new FileReader() 
-            reader.readAsArrayBuffer(file)
+            let reader = new FileReader()
+			
+			// Difference between Switch and other platforms is lack of any encoding, it's just JSON.
+			if (document.getElementById("switch-save").checked) reader.readAsText(file)
+			else reader.readAsArrayBuffer(file)
+		
             reader.addEventListener("load", () => {
-                var result = reader.result 
+                var result = reader.result
                 try {
-                    let decrypted = new Uint8Array(result)
-                    var jsonString = JSON.stringify(JSON.parse(decrypted), undefined, 2)
+					let decrypted = ""
+					if (document.getElementById("switch-save").checked) {
+						decrypted = result
+					}
+					else {
+						decrypted = Decode(new Uint8Array(result))
+					}
+					var jsonString = JSON.stringify(JSON.parse(decrypted), undefined, 2)
                     const hash = Hash(jsonString)
                     history.removeFromHistory(hash)
                     history.addToHistory(jsonString, file.name, hash)
                     history.syncToLocalStorage()
                     this.setGameFile(jsonString, file.name)
                 } catch (err){
-                    window.alert("The file could not decrypted.")
-                    console.warn(err);
+                    //window.alert("The file could not decrypted.")
+                    console.warn(err)
                 } 
                 this.fileInputRef.current.value = null
             })
@@ -55,6 +65,14 @@ class App extends React.Component {
         this.setState({
             gameFile: this.state.gameFileOriginal
         }) 
+    }
+	handleDownloadAsSwitchSave = e => {
+		try {
+            var data = JSON.stringify(JSON.parse(this.state.gameFile))
+            DownloadData(data, "encoded.dat")
+        } catch (err){
+            window.alert("Could not parse valid JSON. Reset or fix.")
+        }
     }
     handleDownloadAsJSON = e => {
         DownloadData(this.state.gameFile, "decoded.json")
@@ -80,14 +98,18 @@ class App extends React.Component {
     render(){
         return <div id="wrapper">
             {this.state.dragging && <div id="cover"></div>}
-            <p id="description">This online utility allows you to modify a Hollow Knight save file. View the source code in the <a href="https://github.com/bloodorca/hollow">github repo</a>.</p>
-            <ul id="instructions">
+            <p id="description">This online utility allows you to modify a Hollow Knight save file. View the source code in the <a href="https://github.com/bloodorca/hollow">github repo</a>. You can also use this to convert your PC save to and from the Nintendo Switch.</p>
+			<ul id="instructions">
                 <li>Make a backup of your original file.</li>
                 <li>Select or drag in the source save file you want to modify.</li>
                 <li>Modify your save file. Ctrl-F / Cmd-F is your best friend.</li>
                 <li>Download your new modifed save file.</li>
             </ul>
-            <button id="file-button" onClick={this.handleFileClick}>Select File</button>
+			<span>
+				<input type="checkbox" id="switch-save"/>
+				<label htmlFor="switch-save">Nintendo Switch Mode</label>
+			</span>
+			<button id="file-button" onClick={this.handleFileClick}>Select File</button>
             <input onChange={e => { this.handleFileChange(this.fileInputRef.current.files) }} id="file-input"  ref={this.fileInputRef} type="file"/>
             {this.state.editing && (
                 <div id="editor-wrapper">
@@ -96,7 +118,8 @@ class App extends React.Component {
                     <div id="editor-buttons">
                         <button onClick={this.handleReset}>reset</button>
                         <button onClick={this.handleDownloadAsJSON}>download as json</button>
-                        <button onClick={this.handleDownload}>download</button>
+						<button onClick={this.handleDownloadAsSwitchSave}>download as Nintendo Switch save file</button>
+                        <button onClick={this.handleDownload}>download as normal save file</button>
                     </div>
                 </div>
             )}
